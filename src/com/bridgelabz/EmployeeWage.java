be19b718cf532b053645bb675350f3df16f51476
Megha <meghajagadale99@gmail.com>
2021-08-12 11:23:15 +0530

    Added: employee wage for multiple company

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -3,11 +3,8 @@ package com.bridgelabz;
 public class EmployeeWage {
     public static final int FULL_TIME = 2;
     public static final int PART_TIME = 1;
-    public static final int EMP_PER_HOUR = 20;
-    public static final int NUM_OF_WORKING_DAYS = 20;
-    public static final int WORKING_HRS_IN_MONTH = 100;
 
-    public static int computeEmpWage() {
+    public static int computeEmpWage(String  company,int EMP_PER_HOUR, int NUM_OF_WORKING_DAYS,int WORKING_HRS_IN_MONTH  ) {
         int empHrs = 0;
         int totalEmpHrs = 0;
         int totalWorkingDays = 0;
@@ -29,12 +26,13 @@ public class EmployeeWage {
             System.out.println("Day#: " + totalWorkingDays + " Emp Hr: " + empHrs);
         }
         int totalEmpWage = totalEmpHrs * EMP_PER_HOUR;
-        System.out.println("Total Emp Wage: " + totalEmpWage);
+        System.out.println("Total Emp Wage For Company: " +company+" is:" + totalEmpWage);
         return totalEmpWage;
     }
 
-    public static void main(String[] args) {
-        computeEmpWage();
+    public static void main(String[] args){
+        computeEmpWage("Dmart",20, 2, 10);
+        computeEmpWage("Reliance", 10, 4, 20);
     }
 
 }

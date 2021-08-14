9fa84a84022673e7e818cff55992dc2e5186c6f2
Megha <meghajagadale99@gmail.com>
2021-08-14 15:23:38 +0530

    Added: employee attendance

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -1,9 +1,15 @@
 package com.bridgelabz;
 
-public class EmployeeWageComputation
-{
-    public static void main(String args[])
-    {
-        System.out.println("Welcome to Employee Wage Computation Program");
+public class EmployeeWageComputation {
+    public static void main(String args[]) {
+        final int FULL_TIME = 1;
+        int empType = (int) (Math.random() * 100) % 2;
+        if (empType == FULL_TIME) {
+            System.out.println("Employee is Present");
+
+        } else {
+            System.out.println("Employee is Absent");
+
         }
     }
+}
